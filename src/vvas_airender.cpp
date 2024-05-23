990494f5f6f5391f73d5c38e9b17044321c136ac
crayson98 <101181426+crayson98@users.noreply.github.com>
2024-05-23 14:47:52 +0200

    Update vvas_airender.cpp

--- src/vvas_airender.cpp
+++ src/vvas_airender.cpp
@@ -414,7 +414,7 @@ overlay_node_foreach (GNode * node, gpointer kpriv_ptr)
               clr.green, clr.red), kpriv->line_thickness, 1, 0);
         
         conv.conv_kernel_init();
-        conv.conv_kernel_run(40, 40, resptr);
+        conv.conv_kernel_run(100, 100, resptr);
       }
 
       if (label_present) {
