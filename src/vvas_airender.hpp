437e64987e8243d9efbcddb3a26de009153b95a2
Bús Benedek <busbenedek88@gmail.com>
2024-05-24 19:51:45 +0200

    opencl

--- src/vvas_airender.hpp
+++ src/vvas_airender.hpp
@@ -19,7 +19,7 @@
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY 1
 
-#include <CL/opencl.hpp>
+#include <CL/cl2.hpp>
 
 struct OpenCLObject{
     cl::Context context;
