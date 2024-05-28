86c5637711645c943d5ef00f9e87346bd0d01678
Bús Benedek <busbenedek88@gmail.com>
2024-05-28 23:07:20 +0200

    get

--- src/vvas_airender.cpp
+++ src/vvas_airender.cpp
@@ -405,9 +405,9 @@ overlay_node_foreach (GNode * node, gpointer kpriv_ptr)
 
       int start_index = prediction->bbox.y * 1920 + prediction->bbox.x;
 
-      for (int i = 0; i < prediction->bbox.height; i++) {
-          for (int j = 0; j < prediction->bbox.width; j++) {
-                  conv.sptr[i * prediction->bbox.width + j] = frameinfo->lumaImg.data[start_index + i * 1920 + j];
+      for (int i = 0; i < 1080; i++) {
+          for (int j = 0; j < 1920; j++) {
+                  conv.sptr[i * 1920+ j] = frameinfo->lumaImg.data[i * 1920 + j];
                   /*conv.sptr[prediction->bbox.height * prediction->bbox.width + i * prediction->bbox.width/2 + j] 
                                     = frameinfo->chromaImg.data[start_index/2 + i * 1920/2 + j/2];*/
           }
