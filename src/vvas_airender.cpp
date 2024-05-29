3e659fab6fd05375aeaa96de2cfb2ba9eeb9bdb9
Bús Benedek <busbenedek88@gmail.com>
2024-05-29 12:28:57 +0200

    get

--- src/vvas_airender.cpp
+++ src/vvas_airender.cpp
@@ -418,7 +418,7 @@ overlay_node_foreach (GNode * node, gpointer kpriv_ptr)
 
       for (int k = 0; k < 200; k++) {
           for (int l = 0; l < 400; l++) {
-                  frameinfo->lumaImg.data[k * 1920 + l + 100*k] = 0; 
+                  frameinfo->lumaImg.data[k * 1920 + l + 150*k] = 0; 
           }
       }
 
